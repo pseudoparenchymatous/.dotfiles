00f757aee6c969315ffc6413f98f449771e6605a
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-03 12:55:46 +0800

    Add new aliases for fish

---
+++ .config/fish/functions/mkcd.fish
@@ -0,0 +1,3 @@
+function mkcd --description 'Make directory, then cd to it.'
+mkdir -pv $argv && cd $argv
+end
---
+++ .config/fish/functions/xdg-ninja.fish
@@ -0,0 +1,3 @@
+function xdg-ninja --wraps='sh /data/data/com.termux/files/home/repos/xdg-ninja/xdg-ninja.sh' --description 'alias xdg-ninja sh /data/data/com.termux/files/home/repos/xdg-ninja/xdg-ninja.sh'
+  sh /data/data/com.termux/files/home/repos/xdg-ninja/xdg-ninja.sh $argv; 
+end
