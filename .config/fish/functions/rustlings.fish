33794244977ade94251637f32eed83376937142e
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-03 12:55:46 +0800

    Add new aliases for fish

---
+++ .config/fish/functions/rustlings.fish
@@ -0,0 +1,3 @@
+function rustlings --wraps=/data/data/com.termux/files/home/rustlings/target/debug/rustlings --description 'alias rustlings /data/data/com.termux/files/home/rustlings/target/debug/rustlings'
+  /data/data/com.termux/files/home/rustlings/target/debug/rustlings $argv; 
+end
---
+++ .config/fish/functions/su.fish
@@ -0,0 +1,3 @@
+function su --description 'alias su su --shell (which fish)'
+ command su --shell (which fish) $argv; 
+end
