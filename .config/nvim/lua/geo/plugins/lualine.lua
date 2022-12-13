41cfa9dab750c998a5604028b112b5ff67f48f2b
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-13 23:16:02 +0800

    Neovim: Indicate when buffer is modified in Lualine and add keymap to toggle between position and progress in Lualine

--- .config/nvim/lua/geo/plugins/lualine.lua
+++ .config/nvim/lua/geo/plugins/lualine.lua
@@ -1,23 +1,36 @@
 local function lsp_name()
-   return vim.lsp.get_active_clients({ bufnr = 0 })[1].name
+    return vim.lsp.get_active_clients({ bufnr = 0 })[1].name
 end
-require 'lualine'.setup {
-   options = {
-      globalstatus = true,
-      always_divide_middle = false,
-      section_separators = { right = '', left = '' },
-   },
-   sections = {
-      lualine_a = { { 'mode', padding = 0, fmt = function() return ' ' end, } },
-      lualine_b = { 'branch', 'diff' },
-      lualine_c = {
-         { 'diagnostics', separator = '', padding = { right = 0, left = 1 } },
-         { lsp_name, separator = '' },
-      },
-      lualine_x = { 'filetype' },
-   },
-   extensions = {
-      'nvim-tree',
-   },
-}
 
+local function is_buf_modified()
+    if vim.api.nvim_get_option_value('modified', {}) then
+        return '[+]'
+    else
+        return ''
+    end
+end
+
+require 'geo.keymaps.plugins.lualine'
+require 'lualine'.setup {
+    options = {
+        globalstatus = true,
+        always_divide_middle = false,
+        section_separators = { right = '', left = '' },
+    },
+    sections = {
+        lualine_a = { { 'mode', padding = 0, fmt = function() return ' ' end, } },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = {
+            { 'diagnostics', separator = '', padding = { right = 0, left = 1 } },
+            { lsp_name, separator = '' }
+        },
+        lualine_x = {
+            { is_buf_modified, separator = '', },
+        },
+        lualine_y = { 'filetype' },
+        lualine_z = { 'progress' },
+    },
+    extensions = {
+        'nvim-tree',
+    },
+}
