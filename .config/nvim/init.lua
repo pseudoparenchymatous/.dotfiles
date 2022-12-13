58f30609269353ef3406cdb37337ba5f8f5e1abd
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-13 15:45:08 +0800

    Adjust format alignment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Disable builtin plugins
-vim.g.loaded_netrwPlugin      = 1
-vim.g.loaded_tarPlugin        = 1
-vim.g.loaded_zipPlugin        = 1
-vim.g.loaded_2html_plugin     = 1
+vim.g.loaded_netrwPlugin  = 1
+vim.g.loaded_tarPlugin    = 1
+vim.g.loaded_zipPlugin    = 1
+vim.g.loaded_2html_plugin = 1
 
 require 'geo.autocommands'
 require 'geo.keymaps'
