f1a288385ce390f2dd1615a0661a9f4357fb26cf
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-13 23:11:29 +0800

    Neovim: Indent config file to 4 spaces

--- .config/nvim/lua/geo/plugins/nvim-tree.lua
+++ .config/nvim/lua/geo/plugins/nvim-tree.lua
@@ -1,27 +1,26 @@
 require 'nvim-tree'.setup {
-   disable_netrw = true,
-   remove_keymaps = true,
-   hijack_cursor = true,
-   create_in_closed_folder = true,
-   notify = {
-      threshold = vim.log.levels.WARN,
-   },
-   view = {
-      hide_root_folder = true,
-      side = 'right',
-   },
-   on_attach = function(bufnr)
-      require 'geo.keymaps.plugins.nvim-tree' (bufnr)
-      require 'geo.autocommands.nvim-tree' (bufnr)
-   end,
-   renderer = {
-      indent_markers = {
-         enable = true,
-      },
-      icons = {
-         show = {
-            folder_arrow = false,
-         }
-      }
-   }
+    disable_netrw = true,
+    remove_keymaps = true,
+    hijack_cursor = true,
+    notify = {
+        threshold = vim.log.levels.WARN,
+    },
+    view = {
+        hide_root_folder = true,
+        side = 'right',
+    },
+    on_attach = function(bufnr)
+        require 'geo.keymaps.plugins.nvim-tree' (bufnr)
+        require 'geo.autocommands.nvim-tree' (bufnr)
+    end,
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+        icons = {
+            show = {
+                folder_arrow = false,
+            }
+        }
+    }
 }
