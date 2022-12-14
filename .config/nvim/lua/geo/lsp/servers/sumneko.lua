c086151bc91ff2b131eeb9f4fadce479fe484a76
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-14 17:07:08 +0800

    Neovim: Enable hints for sumneko

--- .config/nvim/lua/geo/lsp/servers/sumneko.lua
+++ .config/nvim/lua/geo/lsp/servers/sumneko.lua
@@ -11,6 +11,10 @@ require 'lspconfig'.sumneko_lua.setup {
             diagnostics = {
                 globals = { 'vim' }
             },
+            hint = {
+                enable = true,
+                setType = true
+            },
             -- workspace = {
             --     library = vim.api.nvim_get_runtime_file('', true),
             -- },
