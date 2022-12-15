189aaf6f648f7b49d99ac4b4f8d4a0e8ca5361b6
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-15 22:48:07 +0800

    Neovim: Indent config file to 4 spaces

--- .config/nvim/lua/geo/plugins/indentline.lua
+++ .config/nvim/lua/geo/plugins/indentline.lua
@@ -1,7 +1,7 @@
 require 'indent_blankline'.setup {
-   use_treesitter = true,
-   show_current_context = true,
-   -- max_indent_increase = 1,
-   -- char_blankline = 'A',
-   show_trailing_blankline_indent = false,
+    use_treesitter = true,
+    show_current_context = true,
+    -- max_indent_increase = 1,
+    -- char_blankline = 'A',
+    show_trailing_blankline_indent = false,
 }
