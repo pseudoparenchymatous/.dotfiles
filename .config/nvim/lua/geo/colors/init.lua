ff16fab834b3bafad57fc24f98972bbb434c3173
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-20 00:07:37 +0800

    Neovim: Rename `colorschemes` directory to `colors`

--- .config/nvim/lua/geo/colors/init.lua
+++ .config/nvim/lua/geo/colors/init.lua
@@ -1,6 +1,6 @@
 --vim.g.edge_style = 'aura'
 --vim.g.edge_better_performance = 1
 -- require 'geo.colorschemes.gruvbox'
-require 'geo.colorschemes.gruv-material'
+require 'geo.colors.gruv-material'
 
 vim.cmd.colorscheme 'gruvbox-material'
