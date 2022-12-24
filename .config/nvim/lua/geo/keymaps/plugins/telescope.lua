1195b1528de7909b638a6e25f8768efd55e3de0e
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-12-24 20:31:15 +0800

    Neovim: Remap marking selections in Telescope

--- .config/nvim/lua/geo/keymaps/plugins/telescope.lua
+++ .config/nvim/lua/geo/keymaps/plugins/telescope.lua
@@ -4,3 +4,11 @@ local map = vim.keymap.set
 map('n', '<Leader>tf', picker.find_files)
 map('n', '<Leader>tg', picker.live_grep)
 map('n', '<Leader>tp', picker.builtin)
+
+local act = require 'telescope.actions'
+return {
+    i = {
+        ["<Tab>"] = 'move_selection_worse',
+        ["<C-Space>"] = 'toggle_selection',
+    }
+}
