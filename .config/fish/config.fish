ec1d87ba33352fb87d6daf074ea7e6c5a5e90335
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2023-04-24 01:15:45 +0800

    Fish: Add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
+    abbr --add nv nvim
+    abbr --add nvrc 'nvim ~/.dotfiles/.config/nvim'
+    abbr --add zj zellij
+    abbr --add upd nala update
+    abbr --add upg nala upgrade
+    abbr --add i nala install
+
+    alias ls "ls --group-directories-first"
+    abbr --add la ls -A
+    abbr --add ll la -Ggh
 end
